bbdaa3aae0323c41a75749df970f2dfed801f5fc
Jacob Schneider <jakieschneider13@gmail.com>
2024-08-29 14:04:55 +0200

    Some idiot used three-space indentation

--- theme-variables.js
+++ theme-variables.js
@@ -2,16 +2,16 @@
  * Custom function used to generate the output of the theme variables; more about it can be found at https://getpublii.com/dev/theme-variables/
  */
 
-var generateThemeVariables = function(params) {
-   let output = '';
+var generateThemeVariables = function (params) {
+    let output = '';
 
-      output += ` 
-         :root {
-             --main-width:   ${params.mainWidth};
-             --sidebar-width:  ${params.sidebarWidth};
-         }`;  
+    output += ` 
+        :root {
+            --main-width:   ${params.mainWidth};
+            --sidebar-width:  ${params.sidebarWidth};
+        }`;
 
-   return output;
+    return output;
 }
 
 module.exports = generateThemeVariables;
